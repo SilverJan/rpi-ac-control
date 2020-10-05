dedc57fcc056c86d918828725d845cf676e3e3ea
Jan Bissinger (CI/CBW2) <jan.bissinger@sg.bosch.com>
2020-10-05 19:10:12 +0800

    update code

--- src/opt/rpi-ac-control/ac_control_server.py
+++ src/opt/rpi-ac-control/ac_control_server.py
@@ -30,4 +30,4 @@ api.add_resource(AcControl, '/<string:cmd>')
 
 if __name__ == '__main__':
     ac = Mitsubishi(22, LogLevel.ErrorsOnly)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
